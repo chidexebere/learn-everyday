b79831ecd82c44863de177f0cb69d9649b17faf3
Chidi <chidex5@gmail.com>
2020-08-20 23:48:08 +0100

    Add link routes with react router

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import Button from "../elements/Button";
 import Icon from "../elements/Icon";
+import Header from "../layout/Header";
+import Logo from "../components/Logo";
 
-const Home = () => {
+const Home = (props) => {
+	console.log(props);
 	return (
 		<div className="home">
+			<Header type="default">
+				<Logo />
+			</Header>
+
 			<h2 className="home__title">Select Quiz</h2>
 			<div className="buttons">
-				<Button text="Past Questions" type="buttonTitle">
-					<Icon fontType="fa fa-chevron-right" />
-				</Button>
+				<Link to="/list">
+					<Button text="Past Questions" type="buttonTitle">
+						<Icon fontType="fa fa-chevron-right" />
+					</Button>
+				</Link>
 				<Button text="World History" type="buttonTitle">
 					<Icon fontType="fa fa-chevron-right" />
 				</Button>
