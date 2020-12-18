7f1ddc864d2ea78e8fa6ad9fe13aa90f23e1bcab
Chidi <chidex5@gmail.com>
2020-12-18 15:08:54 +0100

    Add help data and functions

---
+++ src/utils/helpers.ts
@@ -0,0 +1,33 @@
+export const questionYearStart = 2000;
+
+export const questionYearEnd = 2019;
+
+export const getQuestionYears = (
+  questionYearStart: number,
+  questionYearEnd: number,
+) => {
+  let years: number[] = [];
+  let startYear = questionYearStart;
+  let endYear = questionYearEnd;
+  years.push(startYear);
+  let lastItem = startYear;
+  while (lastItem < endYear) {
+    years.push(lastItem + 1);
+    lastItem = years[years.length - 1];
+  }
+  return years;
+};
+
+interface Options {
+  a: string;
+  b: string;
+  c: string;
+  d: string;
+}
+
+export const filterAnswer = (options: Options, answer: string) => {
+  const optionKeys = Object.keys(options);
+  const filteredAnswerIndex = optionKeys.indexOf(answer);
+  const optionValues = Object.values(options);
+  return optionValues[filteredAnswerIndex];
+};
