02f4c51858e1d7206c2b177ba1dbb52bf306449d
Chidi <chidex5@gmail.com>
2021-11-26 06:28:34 +0100

    Wrap SW registration call in load for better performance

--- public/js/register_sw.js
+++ public/js/register_sw.js
@@ -1,10 +1,12 @@
 /* Register service worker */
+// Wrapping the SW registration call in load provides better performance
+// See: https://developers.google.com/web/fundamentals/primers/service-workers/registration
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
+window.addEventListener('load', () => {
+  if ('serviceWorker' in navigator) {
     navigator.serviceWorker
       .register('./sw.js')
       .then((reg) => console.log('SW Registered!', reg.scope))
       .catch((err) => console.log('SW registration failed!', err));
-  });
-}
+  }
+});
